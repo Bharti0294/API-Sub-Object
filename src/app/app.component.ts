9e559cb51e33e22a9e3149934374d70dc730ccaa
Bharti0294 <bharti.nehul07@gmail.com>
2021-04-08 10:24:59 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'APISubObject';
+
+  apiUrl = 'http://jsonplaceholder.typicode.com/users';
+
+  apiData: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  ngOnInit() {
+    this.http.get(this.apiUrl).subscribe((data) => {
+      console.warn(data)
+      this.apiData = data;
+    })
+  }
 }
